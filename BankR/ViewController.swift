5b4a9d4dbc6ba39582c2bce53204f2e5f8bd2fcb
Sanesh Gobin <saneshgobin@icloud.com>
2023-07-20 18:26:15 +0200

    Initial Commit

---
+++ BankR/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BankR
+//
+//  Created by Sanesh Gobin on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "loggedIn", sender: sender)
+    }
+    
+}
+

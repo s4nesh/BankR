913b74beeb980560aaa4876192d1783fb80d4ee3
Sanesh Gobin <saneshgobin@icloud.com>
2023-07-22 17:45:32 +0200

    Add textfield and sub class view controller
    On the sign up screen I added the text fields and button.
    Also created a file and in it created a sub class of view controller.
    Linked the sign up screen to signup view controller file.

---
+++ BankR/SignupViewcontroller.swift
@@ -0,0 +1,15 @@
+//
+//  SignupViewcontroller.swift
+//  BankR
+//
+//  Created by Sanesh Gobin on 2023/07/22.
+//
+
+import Foundation
+import UIKit
+
+class SignupViewcontroller: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

f395a8e1cf0329b61e3455afc3bdb17ee64c03e3
Sanesh Gobin <saneshgobin@icloud.com>
2023-07-27 18:07:08 +0200

    Add login view controller file and link to storyboard login screen

---
+++ BankR/LogInViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LogInViewController.swift
+//  BankR
+//
+//  Created by Sanesh Gobin on 2023/07/27.
+//
+
+import Foundation
+import UIKit
+
+class LogInViewController: UIViewController, UITextFieldDelegate {
+     override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
